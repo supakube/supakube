2f2c8f585596e249dd7cd76030fde89cfec7aca7
$NAME <$EMAIL>
2024-11-12 13:14:19 +0000

    Hot deploy

--- crates/web-server/src/main.rs
+++ crates/web-server/src/main.rs
@@ -1,12 +1,24 @@
+use std::env;
+
 use axum::{response::Html, routing::get, Router};
+use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
     // build our application with a route
     let app = Router::new().route("/", get(handler));
 
+    // Get the port from the environment variable, default to 3000 if not set
+    let port: u16 = env::var("PORT")
+        .unwrap_or_else(|_| "3000".to_string())
+        .parse()
+        .expect("PORT must be a valid u16 number");
+
+    // Define the address to listen on (0.0.0.0)
+    let addr = SocketAddr::from(([0, 0, 0, 0], port));
+
     // run it
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+    let listener = tokio::net::TcpListener::bind(&addr)
         .await
         .unwrap();
     println!("listening on {}", listener.local_addr().unwrap());
@@ -14,5 +26,5 @@ async fn main() {
 }
 
 async fn handler() -> Html<&'static str> {
-    Html("<h1>Hello, World!</h1>")
+    Html("<h1>Hello, World9!</h1>")
 }
