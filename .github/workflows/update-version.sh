c753ba93adc87f699b0dc2c14c8783efc901b7e7
$NAME <$EMAIL>
2024-11-16 12:49:00 +0000

    fix: Set version

--- .github/workflows/update-version.sh
+++ .github/workflows/update-version.sh
@@ -2,7 +2,7 @@
 
 # Remove a leading v if there is one
 VERSION=$(echo $1 | sed 's/v//g')
-sed -i "0,/version/{s/version.*$/version = \"$VERSION\"/}" ../../supakube/Cargo.toml
+sed -i "0,/version/{s/version.*$/version = \"$VERSION\"/}" ../../crates/supakube/Cargo.toml
 
 # Update all the version number of the bionic operator
 sed -i "/name = \"supakube\"/{n;s/.*/version = \"$VERSION\"/}" ../../Cargo.lock
