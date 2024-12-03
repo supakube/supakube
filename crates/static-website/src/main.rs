d1753f583fb6a6bae491e83544da65020fc16aaa
$NAME <$EMAIL>
2024-12-03 12:17:23 +0000

    Hot reload

--- crates/static-website/src/main.rs
+++ crates/static-website/src/main.rs
@@ -11,6 +11,7 @@ use axum::Router;
 use dioxus::prelude::{ComponentFunction, Element, VirtualDom};
 use std::{fs, net::SocketAddr, path::Path};
 use tower_http::services::ServeDir;
+use tower_livereload::LiveReloadLayer;
 
 pub mod routes {
 
@@ -89,7 +90,9 @@ async fn main() {
         let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
 
         // build our application with a route
-        let app = Router::new().nest_service("/", ServeDir::new("dist"));
+        let app = Router::new()
+            .nest_service("/", ServeDir::new("dist"))
+            .layer(LiveReloadLayer::new());
 
         let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
         tracing::info!("listening on http://{}", &addr);
